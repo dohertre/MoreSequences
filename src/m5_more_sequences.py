248c0f41f44c0ead0efc56dfbd5760aa23e47398
dohertre <dohertre@rose-hulman.edu>
2019-06-28 20:47:22 -0400

    rand

--- src/m5_more_sequences.py
+++ src/m5_more_sequences.py
@@ -239,9 +239,16 @@ def index_of_first_negative(numbers):
       :rtype: int
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+    for k in range(len(numbers)):
+        if numbers[k] < 0:
+            return k
+        elif numbers[k] >= 0:
+            k = k + 1
+            if k == len(numbers):
+                return -1
 
 
 def run_test_contains_an_a():
